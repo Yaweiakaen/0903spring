278a4371d609f399d95799133ef3613fa058f5d9
student <student@DESKTOP-4ARRLB2>
2022-09-03 12:09:37 +0800

    ㄋ

--- spring0903/src/main/java/demo/springmvc/RequestParamController.java
+++ spring0903/src/main/java/demo/springmvc/RequestParamController.java
@@ -2,13 +2,14 @@ package demo.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
  
 @Controller
 public class RequestParamController {
      
-    @RequestMapping("/user")
-    public String userInfo(Model model, @RequestParam(value = "name", defaultValue = "Guest") String name) {
+    @RequestMapping(value="/user", method = RequestMethod.POST)
+    public String userInfo(Model model, @RequestParam(value = "name", defaultValue = "Guest") String name ) {
  
         model.addAttribute("name", name);
  
@@ -18,6 +19,14 @@ public class RequestParamController {
             model.addAttribute("email", "Not set");
         }
         return "userInfo";
+    }
+    
+    @RequestMapping("/userui")
+    public String userUI(Model model) {
+ 
+        model.addAttribute("username", "admin"); 
+      
+        return "userform";
     }
   
 }
---
+++ spring0903/src/main/webapp/WEB-INF/views/userform.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>User Form</title>
+</head>
+<body>
+<form action="user" method="post">
+    User Name:<input  type="text"  name="name" value="${username}"/><br/>
+    <input type="submit"/>
+</form>
+</body>
+</html>
